072a07c56ed3f5b86e9f6af47a452b64c10b12ee
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-05-24 10:55:43 +0300

    Prisma ORM init

---
+++ prisma/migrations/20240524075158_current_ver/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "created_by" INTEGER NOT NULL,
+    "post" TEXT NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "hashed_password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
 
-app.get("/", function (req, res) {
+const app: Application = express();
+const PORT: string | number = process.env.PORT || 5000;
+
+app.get("/", function (req: Request, res: Response) {
     res.send("Hello World");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log("Server works on port " + PORT);
+});

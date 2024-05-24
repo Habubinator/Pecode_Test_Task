831fa690cc84f237279768a74c4130bd39113b56
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-05-24 11:33:07 +0300

    jwt middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import checkToken from "./middlewares/jwt.ts";
 
 const app: Application = express();
+app.use(checkToken);
 const PORT: string | number = process.env.PORT || 5000;
 
-app.get("/", function (req: Request, res: Response) {
+app.get("/user", function (req: Request, res: Response) {
     res.send("Hello World");
 });
 

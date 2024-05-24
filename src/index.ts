afb0e2615beb349af8a481bbe38e3bb2f9c8fde6
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-05-24 14:58:50 +0300

    dumb error update

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,12 @@ import postRouter from "./routes/postRouter.js";
 import checkToken from "./middlewares/jwt.js";
 
 const app: Application = express();
+app.use(express.json());
 app.use(checkToken);
 const PORT: string | number = process.env.PORT || 5000;
 
-app.get("/user", userRouter as Router);
-app.get("/post", postRouter as Router);
+app.use("/user", userRouter as Router);
+app.use("/post", postRouter as Router);
 
 app.listen(PORT, () => {
     console.log("Server works on port " + PORT);
